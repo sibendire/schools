5d113807c588b9004deac8beedf6239a26f5cab7
JOSH <sibendirejoshua@gmail.com>
2023-11-13 11:32:03 +0300

    created senior two table class

---
+++ src/main/java/com/schools/school/repository/SeniorTwoRepository.java
@@ -0,0 +1,7 @@
+package com.schools.school.repository;
+
+import com.schools.school.entity.SeniorTwo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeniorTwoRepository extends JpaRepository<SeniorTwo,Long> {
+}
---
+++ src/main/java/com/schools/school/service/serviceImpl/SeniorTwoServiceImpl.java
@@ -0,0 +1,6 @@
+package com.schools.school.service.serviceImpl;
+
+import com.schools.school.service.SeniorTwoService;
+
+public class SeniorTwoServiceImpl implements SeniorTwoService {
+}
