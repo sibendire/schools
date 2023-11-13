28b5cb270e270c29148abe4f88410e7ae1bfc78a
JOSH <sibendirejoshua@gmail.com>
2023-11-13 11:50:16 +0300

    created  senior three class table

---
+++ src/main/java/com/schools/school/service/serviceImpl/SeniorThreeServiceImpl.java
@@ -0,0 +1,8 @@
+package com.schools.school.service.serviceImpl;
+
+import com.schools.school.service.SeniorThreeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SeniorThreeServiceImpl implements SeniorThreeService {
+}
