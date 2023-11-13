f43f31c5dd4b476d661e5d327eca738d99c4ec87
JOSH <sibendirejoshua@gmail.com>
2023-11-13 11:35:29 +0300

    Updated two classes

--- src/main/java/com/schools/school/repository/SeniorTwoRepository.java
+++ src/main/java/com/schools/school/repository/SeniorTwoRepository.java
@@ -2,6 +2,9 @@ package com.schools.school.repository;
 
 import com.schools.school.entity.SeniorTwo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface SeniorTwoRepository extends JpaRepository<SeniorTwo,Long> {
 }
--- src/main/java/com/schools/school/service/serviceImpl/SeniorTwoServiceImpl.java
+++ src/main/java/com/schools/school/service/serviceImpl/SeniorTwoServiceImpl.java
@@ -1,6 +1,8 @@
 package com.schools.school.service.serviceImpl;
 
 import com.schools.school.service.SeniorTwoService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SeniorTwoServiceImpl implements SeniorTwoService {
 }
